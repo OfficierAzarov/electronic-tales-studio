0296626bd7125bee995f2b2190a9b0f6e3bc5984
Officier Azarov <elisabethfainstein@gmail.com>
2023-05-16 20:07:04 +0200

    (feat) add fundamental sturcture

---
+++ pages/articles/[articleId].tsx
@@ -0,0 +1,9 @@
+const ArticlePage = (): JSX.Element => {
+  return (
+    <>
+      <h1>Qu'est-ce qu'un compilateur ?</h1>
+    </>
+  );
+};
+
+export default ArticlePage;
---
+++ pages/articles/index.tsx
@@ -0,0 +1,13 @@
+const ArticlesPage = (): JSX.Element => {
+  return (
+    <>
+      <h1>Articles</h1>
+      <ul>
+        <li>Article 1</li>
+        <li>Article 2</li>
+      </ul>
+    </>
+  );
+};
+
+export default ArticlesPage;
